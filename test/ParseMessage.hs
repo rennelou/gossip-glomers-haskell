672a461cbe28370d13d536ba945a577294f3f1a0
rlou <rlou@americastg.com>
2023-06-02 15:21:54 -0300

    testes do json parse de Message

--- test/ParseMessage.hs
+++ test/ParseMessage.hs
@@ -25,34 +25,68 @@ test_parseInit =
         dest = "n1",
         body = Init { msg_id = 1, node_id = "n3", node_ids = ["n1", "n2", "n3"]}
       }
-  in assertEqual message (strDecodeMessage jsonMessage)
+  in do assertMessageDecode message jsonMessage
+        assertMessageIsomorphism message
 
 test_parseInit_Ok :: IO ()
 test_parseInit_Ok =
-  let message = Message {
-    src = "Joe",
-    dest = "12",
-    body = Init_Ok { in_reply_to = 1 }
-  } 
-  in assertEqual message (decodeMessage . encodeMessage $ message)
+  let jsonMessage = "{\
+        \\"src\":\"c1\"\
+        \,\"dest\":\"n1\"\
+        \,\"body\":{\
+        \  \"type\":\"init_ok\"\
+        \ ,\"in_reply_to\":1\
+        \}}"
+      message = Message {
+        src = "c1",
+        dest = "n1",
+        body = Init_Ok { in_reply_to = 1 }
+      } 
+  in do assertMessageDecode message jsonMessage
+        assertMessageIsomorphism message
 
 test_parseEcho :: IO ()
 test_parseEcho =
-  let message = Message {
-    src = "Joe",
-    dest = "12",
-    body = Echo { msg_id = 1, echo = "Nothing" }
-  } 
-  in assertEqual message (decodeMessage . encodeMessage $ message)
+  let jsonMessage = "{\
+        \\"src\":\"c1\"\
+        \,\"dest\":\"n1\"\
+        \,\"body\":{\
+        \  \"type\":\"echo\"\
+        \ ,\"msg_id\":1\
+        \ ,\"echo\":\"Please echo 35\"\
+        \}}"
+      message = Message {
+        src = "c1",
+        dest = "n1",
+        body = Echo { msg_id = 1, echo = "Please echo 35" }
+      } 
+  in do assertMessageDecode message jsonMessage
+        assertMessageIsomorphism message
 
 test_parseEcho_Ok :: IO ()
 test_parseEcho_Ok =
-  let message = Message {
-    src = "Joe",
-    dest = "12",
-    body = Echo_Ok { msg_id = 1, in_reply_to = 1, echo = "Nothing" }
+  let jsonMessage = "{\
+        \\"src\":\"c1\"\
+        \,\"dest\":\"n1\"\
+        \,\"body\":{\
+        \  \"type\":\"echo_ok\"\
+        \ ,\"msg_id\":2\
+        \ ,\"in_reply_to\":1\
+        \ ,\"echo\":\"Please echo 35\"\
+        \}}"
+      message = Message {
+    src = "c1",
+    dest = "n1",
+    body = Echo_Ok { msg_id = 2, in_reply_to = 1, echo = "Please echo 35" }
   } 
-  in assertEqual message (decodeMessage . encodeMessage $ message)
+  in do assertMessageDecode message jsonMessage
+        assertMessageIsomorphism message
+
+assertMessageDecode :: Message -> String -> IO ()
+assertMessageDecode expected jsonMessage = assertEqual expected (strDecodeMessage jsonMessage)
+
+assertMessageIsomorphism :: Message -> IO ()
+assertMessageIsomorphism message = assertEqual message (decodeMessage . encodeMessage $ message)
 
 encodeMessage :: Message -> BL.ByteString
 encodeMessage = encode
@@ -62,9 +96,6 @@ decodeMessage b =
   case eitherDecode b of
     Left e -> error e
     Right message -> message
-
-strEncodeMessage :: Message -> String
-strEncodeMessage = TL.unpack . TL.decodeUtf8 . encode
 
 strDecodeMessage :: String -> Message
 strDecodeMessage s =  
